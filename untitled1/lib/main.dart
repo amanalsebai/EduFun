b96a1da9ab636b8b57e486f2fa118cb595f0cc92
amanalsebai <amanalsebai25@gmail.com>
2026-07-08 11:09:26 +0300

    save all changes and progress

--- untitled1/lib/main.dart
+++ untitled1/lib/main.dart
@@ -3,9 +3,9 @@ import 'app.dart';
 import 'core/utils/audio_manager.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized(); //تهيئة Flutter قبل استخدام خدمات النظام
   await AudioManager.playBackgroundMusic(); // تشغيل الموسيقى عند الإقلاع
-  runApp(SmartGamesApp());
+  runApp(SmartGamesApp()); //تشغيل التطبيق وعرض الواجهة الرئيسية
 }
 
 
